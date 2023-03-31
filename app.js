3c3f284fc9e604d21dff365b28a994d4dba7429d
fayyazbaig1 <fayyazbaig124@gmail.com>
2023-03-31 23:17:31 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,46 @@
+alert ("asslam.o.alaikum") ;
+
+console.log("ramadan mubarak")
+
+var a = 10;
+var b = 20;
+var c = 10;
+var d = a + b - c;
+
+console.log(d)
+
+var ali = 50;
+var asif = 100;
+var ghani = 20;
+var empire = ali+asif-ghani;
+
+console.log(empire)
+
+var one = 50;
+var two = 75;
+var three = one*two;
+
+console.log(three)
+
+var lala = ("10");
+var bala = 30;
+var dada = lala+bala;
+
+console.log (dada);
+
+var firstname = prompt("enter your firstname");
+var lastname = prompt ("enter your last name");
+var age = prompt("enter your age");
+var city = prompt ("enter your city", "karachi")
+
+console.log (firstname + " " + lastname + " " + age + " " + "from" + " " + city )
+ 
+var bahigharparhai = false;
+
+if (bahigharparhai== true) {
+    console.log ("aajao");
+}
+
+else {
+console.log ("khud chale jao");
+}
